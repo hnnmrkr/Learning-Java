fdec43853233d74e8e5b402a7b3b097c9a60dc34
Hanna <hanna-marii.kriisa@voco.ee>
2023-10-10 13:21:45 +0300

    Week 7

---
+++ src/Week7/Ex4/Thing.java
@@ -0,0 +1,29 @@
+package Week7.Ex4;
+
+public class Thing {
+  private String name;
+  private int weight;
+
+  public Thing(String name, int weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+
+
+  public String toString() {
+    return name + " (" + weight + " kg)";
+  }
+}
