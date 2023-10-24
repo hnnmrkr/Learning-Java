35d182c4884c682af5d6477e88bbe7df891aa718
Hanna <hanna-marii.kriisa@voco.ee>
2023-10-24 09:42:21 +0300

    Week 10

---
+++ src/Week10/Main.java
@@ -0,0 +1,10 @@
+package Week10;
+
+public class Main {
+  public static void main(String[] args) {
+    Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+    hamburger.addHamburgerAddition1("Tomato", 0.27);
+    hamburger.addHamburgerAddition2("Lettuce", 0.75);
+    hamburger.addHamburgerAddition3("Cheese", 1.13);
+  }
+}
